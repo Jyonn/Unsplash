37c49456e320ffee72840b5d75e1866cde61f0d2
jyonnliu <i@6-79.cn>
2025-08-11 13:44:23 +0800

    use v4 packer

--- Unsplash/settings.py
+++ Unsplash/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'SmartDjango.middleware.HttpPackMiddleware',
+    'smartdjango.middleware.APIPacker',
 ]
 
 
